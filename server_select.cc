041237a6af3d4b96553718b1d0290a30b61783fa
bigheadofliu <871314635@qq.com>
2026-01-28 10:25:48 +0800

    update

--- server_select.cc
+++ server_select.cc
@@ -1,6 +1,7 @@
 /**
  *  select模式的 socket server客户端
  *  改自 server.cpp
+ *  v1.0 单线程版本
  */
 #include<iostream>
 #include<cstring>
@@ -9,77 +10,7 @@
 #include<sys/socket.h>
 #include<unistd.h> 
 #include<sys/select.h> //using select
-#include<thread> //using thread
-#include<memory> //using make_share
-#include<mutex> 
-#include<atomic>
 
-std::mutex maxfd_metux;
-std::mutex retset_mutex;
-
-struct fd_info{
-    int fd;
-    int& max_fd;  //需要修改（传入传出-用指针） 指针太麻烦 改引用了
-    fd_set& red_set; //需要修改（传入传出-用指针）
-};
-
-void accept_thread(/*fd_info* info*/const std::shared_ptr<fd_info> info){
-    std::cout<<" accept子线程开始执行，线程 id："<<std::this_thread::get_id()<<std::endl;
-    int cfd=accept(info->fd,nullptr,nullptr);
-    if(cfd==-1){
-        perror("accept");
-        return;
-    }
-    retset_mutex.lock();
-    // std::lock_guard<std::mutex> locker(retset_mutex);
-    FD_SET(cfd,&info->red_set);
-    retset_mutex.unlock();
-
-    maxfd_metux.lock();
-    info->max_fd=cfd>info->max_fd?cfd:info->max_fd;   //更新 maxfd
-    maxfd_metux.unlock();
-}
-
-void comm_thread(/*fd_info* info*/const std::shared_ptr<fd_info> info){
-    std::cout<<" 通信子线程开始执行，线程 id："<<std::this_thread::get_id()<<std::endl;
-    // 开始收发数据(拷贝阻塞模式的读写)
-    char buffer[1024] {};
-
-    //读数据
-    // int ret_read=read(i,buffer,sizeof(buffer));
-    int ret_read=recv(info->fd,buffer,sizeof(buffer),0);  //改用 recv
-    if(ret_read>0){
-        //有数据可读
-        std::cout<<"收到客户端消息："<<buffer<<std::endl;
-    }else if(ret_read==0){
-        //无数据可读
-        std::cout<<"已断开连接！"<<std::endl;
-        //断开连接需要删除集合中的文件描述符
-        retset_mutex.lock();
-        FD_CLR(info->fd,&info->red_set);
-        retset_mutex.unlock();
-        close(info->fd);
-        // break;  不应该使用跳出循环（多客户端情况下） 应该进行下次检测
-        // continue;
-        return; //退出子线程
-    }else{  /* -1 */
-        //error
-        perror("read");
-        exit(1);
-    }
-    //发数据
-    std::fill(std::begin(buffer),std::end(buffer),0);  //清空缓冲区
-    std::string msg="服务器确认收到消息:"/*+std::to_string(number++)8*/;  
-    strcpy(buffer,msg.c_str());  //拷贝消息到缓冲区
-    // int ret_write=write(i,buffer,strlen(buffer));
-    int ret_write=send(info->fd,buffer,strlen(buffer),0);  //非阻塞改用 send
-    if(ret_write==-1){
-        perror("write");
-        // break;
-    }
-    // sleep(1); //避免发送频繁看不到 阻塞模式 可以用 非阻塞不要用
-    return;
-}
 
 int main(){
         //1.创建用于监听的套接字
@@ -126,9 +57,9 @@ int main(){
         int maxfd=lfd;      //指定 select 存储的最大的套接字下标（上限为 1024）
         
         while(1){
-            retset_mutex.lock();
+ 
             fd_set temp_set=red_set;  //拷贝一份
-            retset_mutex.unlock();
+     
             
             int ret_select=select(maxfd+1,&temp_set,nullptr,nullptr,nullptr);
             if(ret_select==-1){
@@ -136,112 +67,53 @@ int main(){
                 break;
             }
             if(FD_ISSET(lfd,&temp_set)){
-                // 放进子线程处理
+             
+                int cfd=accept(lfd,nullptr,nullptr);
+                if(cfd==-1){
+                    perror("accept");
+                }
 
-                // int cfd=accept(lfd,nullptr,nullptr);
-                // if(cfd==-1){
-                //     perror("accept");
-                //     continue;
-                // }
-                // FD_SET(cfd,&red_set);
-                // maxfd=cfd>maxfd?cfd:maxfd;   //更新 maxfd
-
-                // fd_info info={lfd,maxfd,red_set};  //注意变量生存周期 线程间调用必须使用堆区内存
-                // fd_info* info=new fd_info{lfd,maxfd,red_set}; //不想手动管理内存？ 
-                std::shared_ptr<fd_info> info(new fd_info{lfd,maxfd,red_set});
-                std::thread t1(accept_thread,info);
-                t1.detach();
+                FD_SET(cfd,&red_set);
+                maxfd=cfd>maxfd?cfd:maxfd;   //更新 maxfd
+                
+      
             }
             for(auto i=0;i<=maxfd;++i){ //遍历 fd_set
                 if(i!=lfd && FD_ISSET(i,&temp_set)){ //不是监听 lfd 剩下的就是 cfd
-                    std::shared_ptr<fd_info> info(new fd_info{i,maxfd,red_set});   //maxfd可以不传 但是定义的引用
-                    std::thread t2(comm_thread,info);
-                    t2.detach();
-                //     // 收发数据放入子线程
-                //     // 开始收发数据(拷贝阻塞模式的读写)
-                //     char buffer[1024] {};
-
-                //     //读数据
-                //     // int ret_read=read(i,buffer,sizeof(buffer));
-                //     int ret_read=recv(i,buffer,sizeof(buffer),0);  //改用 recv
-                //     if(ret_read>0){
-                //         //有数据可读
-                //         std::cout<<"收到客户端消息："<<buffer<<std::endl;
-                //     }else if(ret_read==0){
-                //         //无数据可读
-                //         std::cout<<"已断开连接！"<<std::endl;
-                //         //断开连接需要删除集合中的文件描述符
-                //         FD_CLR(i,&red_set);
-                //         close(i);
-                //         // break;  不应该使用跳出循环（多客户端情况下） 应该进行下次检测
-                //         continue;
-                //     }else{  /* -1 */
-                //         //error
-                //         perror("read");
-                //         exit(1);
-                //     }
-                //     //发数据
-                //     std::fill(std::begin(buffer),std::end(buffer),0);  //清空缓冲区
-                //     std::string msg="服务器确认收到消息:"/*+std::to_string(number++)8*/;  
-                //     strcpy(buffer,msg.c_str());  //拷贝消息到缓冲区
-                //     // int ret_write=write(i,buffer,strlen(buffer));
-                //     int ret_write=send(i,buffer,strlen(buffer),0);  //非阻塞改用 send
-                //     if(ret_write==-1){
-                //         perror("write");
-                //         break;
-                //     }
-                //     // sleep(1); //避免发送频繁看不到 阻塞模式 可以用 非阻塞不要用
-                // }
+                     // 开始收发数据(拷贝阻塞模式的读写)
+                    char buffer[1024] {};
+                    //读数据
+                    int ret_read=recv(i,buffer,sizeof(buffer),0);  //改用 recv
+                    if(ret_read>0){
+                        //有数据可读
+                        std::cout<<"收到客户端消息："<<buffer<<std::endl;
+                    }else if(ret_read==0){
+                        //无数据可读
+                        std::cout<<"已断开连接！"<<std::endl;
+                        //断开连接需要删除集合中的文件描述符
+                        FD_CLR(i,&red_set);
+                        close(i);
+                    }else{  /* -1 */
+                        //error
+                        perror("read");
+                        exit(1);
+                    }
+                    //发数据
+                    std::fill(std::begin(buffer),std::end(buffer),0);  //清空缓冲区
+                    std::string msg="服务器确认收到消息:"/*+std::to_string(number++)8*/;  
+                    strcpy(buffer,msg.c_str());  //拷贝消息到缓冲区
+                    int ret_write=send(i,buffer,strlen(buffer),0);  //非阻塞改用 send
+                    if(ret_write==-1){
+                        perror("write");
+                        break;
+                    }
+                }
             }
-
-        // sockaddr_in client_socket_addr; //用于存储监听到客户端的地址信息
-        // socklen_t client_len=sizeof(client_socket_addr);  //socket 地址长度信息 用于参数传入
-        // int cfd=accept(lfd,(sockaddr*)&client_socket_addr,&client_len);  //accept用于获取用于通信的套接字
-        // if(cfd==-1){
-        //     perror("accept");
-        // }
-        // char ip[32]{0};
-        // std::cout<<"监听到的客户端";
-        // std::cout<<"打印监听到的客户端信息："<<std::endl;
-        // std::cout<<"端口："<<ntohs(client_socket_addr.sin_port)<<std::endl;
-        // std::cout<<"IP:"<<inet_ntop(AF_INET,&client_socket_addr.sin_addr.s_addr,ip,sizeof(ip));
-
-        // //5.（阻塞模式此步骤开始传输数据）
-        // int number {0};
-        // while (1)//循环收发收据
-        // {   char buffer[1024] {};
-
-        //     //读数据
-        //     int ret_read=read(cfd,buffer,sizeof(buffer));
-        //         if(ret_read>0){
-        //             //有数据可读
-        //             std::cout<<"收到客户端消息："<<buffer<<std::endl;
-        //         }else if(ret_read==0){
-        //             //无数据可读
-        //             std::cout<<"已断开连接！"<<std::endl;
-        //             break;
-        //         }else{
-        //             //error
-        //             perror("read");
-        //             break;
-        //         }
-        //     //发数据
-        //     std::fill(std::begin(buffer),std::end(buffer),0);  //清空缓冲区
-        //     auto msg="服务器确认收到消息:"+std::to_string(number++);  
-        //     strcpy(buffer,msg.c_str());  //拷贝消息到缓冲区
-        //     int ret_write=write(cfd,buffer,strlen(buffer));
-        //     if(ret_write==-1){
-        //         perror("write");
-        //         break;
-        //     }
-        //     sleep(1); //避免发送频繁看不到
-        // }
-        // close(cfd);
         }
-
-    }
     close(lfd);
     return 0;
-}
+    }
+    
+
 
 
